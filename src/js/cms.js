5af0aab794eb0d1eea8d5effcc370b71ccdcac0b
4everunifiedinc <4everunifiedinc@gmail.com>
2020-08-26 22:56:49 -0400

    ship it

--- src/js/cms.js
+++ src/js/cms.js
@@ -6,14 +6,12 @@ import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../c
 
 import HomePreview from "./cms-preview-templates/home";
 import PostPreview from "./cms-preview-templates/post";
-import MissionPreview from "./cms-preview-templates/mission";
 import ValuesPreview from "./cms-preview-templates/values";
 import ContactPreview from "./cms-preview-templates/contact";
 
 CMS.registerPreviewStyle(styles, { raw: true });
 CMS.registerPreviewTemplate("home", HomePreview);
 CMS.registerPreviewTemplate("post", PostPreview);
-CMS.registerPreviewTemplate("mission", MissionPreview);
 CMS.registerPreviewTemplate("values", ValuesPreview);
 CMS.registerPreviewTemplate("contact", ContactPreview);
 CMS.init();
